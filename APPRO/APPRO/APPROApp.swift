7fe3878daef582a3ac23d0003245de827524b5cb
Sangyun Jeong <jsangyun1039@naver.com>
2024-10-08 19:55:17 +0900

    프로젝트 생성

---
+++ APPRO/APPRO/APPROApp.swift
@@ -0,0 +1,34 @@
+//
+//  APPROApp.swift
+//  APPRO
+//
+//  Created by 정상윤 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct APPROApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
---
+++ APPRO/APPROTests/APPROTests.swift
@@ -0,0 +1,17 @@
+//
+//  APPROTests.swift
+//  APPROTests
+//
+//  Created by 정상윤 on 10/8/24.
+//
+
+import Testing
+@testable import APPRO
+
+struct APPROTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
