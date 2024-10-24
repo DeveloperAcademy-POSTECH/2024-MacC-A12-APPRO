0b0b2784853a7ce195d8da5406f5b9cf9cdd2ed6
Sangyun Jeong <jsangyun1039@naver.com>
2024-10-24 13:29:18 +0900

    Feat: appPhase onChange 구현

--- APPRO/APPRO/APPROApp.swift
+++ APPRO/APPRO/APPROApp.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 @main
 struct APPROApp: App {
+    
+    @Environment(\.openImmersiveSpace) private var openImmersiveSpace
+    @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
+    @Environment(\.openWindow) private var openWindow
+    @Environment(\.dismissWindow) private var dismissWindow
 
     @State private var appState = AppState()
 
@@ -19,16 +24,41 @@ struct APPROApp: App {
         }
         .windowStyle(.plain)
         .windowResizability(.contentSize)
+        .onChange(of: appState.appPhase) { _, newPhase in
+            switch newPhase {
+            case .choosingStretchingPart:
+                configureChoosingStretchingPartScene()
+            case .isStretching(_):
+                configureStretchingScene()
+            }
+        }
         
-        WindowGroup(id: appState.stretchingPartsWindowID) {
+        WindowGroup(id: appState.stretchingProcessWindowID) {
             // TODO: 스트레칭 진행 윈도우 구현 및 추가
         }
         .windowStyle(.plain)
         .windowResizability(.contentSize)
         
         ImmersiveSpace(id: appState.immersiveSpaceID) {
-            
+            // TODO: 각 스트레칭에 따른 뷰 추가
         }
         .immersionStyle(selection: .constant(.mixed), in: .mixed)
     }
+    
+    private func configureChoosingStretchingPartScene() {
+        dismissWindow(id: appState.stretchingProcessWindowID)
+        Task {
+            await dismissImmersiveSpace()
+        }
+        openWindow(id: appState.stretchingPartsWindowID)
+    }
+    
+    private func configureStretchingScene() {
+        dismissWindow(id: appState.stretchingPartsWindowID)
+        Task {
+            await openImmersiveSpace(id: appState.immersiveSpaceID)
+        }
+        openWindow(id: appState.stretchingProcessWindowID)
+    }
+    
 }
