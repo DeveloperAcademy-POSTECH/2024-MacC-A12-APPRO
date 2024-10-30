98ffcd6a054c4159d2327ae9b0ec738cb7afd248
daminoworld <ekadls757@gmail.com>
2024-10-30 19:15:47 +0900

    Add: Add ShoulderStretchingView

---
+++ APPRO/APPRO/Views/Stretchings/Shoulder/ShoulderStretchingView.swift
@@ -0,0 +1,18 @@
+//
+//  ShoulderStretchingView.swift
+//  APPRO
+//
+//  Created by Damin on 10/30/24.
+//
+
+import SwiftUI
+
+struct ShoulderStretchingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShoulderStretchingView()
+}
