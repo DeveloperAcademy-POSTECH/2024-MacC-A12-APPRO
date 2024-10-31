4ca4ab51dbf7048f6f1368c6eb77148205591702
daminoworld <ekadls757@gmail.com>
2024-10-31 16:16:36 +0900

    Feat: Compute transform of handTracking
    - 좌우 스트레칭의 완료여부를 isRightDone으로 판별하여 오른쪽 스트레칭 오른쪽 핸드앵커의 transform을 getTransform으로 업데이트 시켜주고
    - isFirstPositioning으로 처음에만 포물선을 다시 만들 수 있도록 하고 isFistShowing으로 주먹을 쥔 상태인지 아닌지를 판별한다
    - 주먹을 쥔 상태인지 아닌지는 손의 thumbIntermediateBase, indexFingerIntermediateTip 앵커 사이의 거리로 판별하여 거리가 0.1 이상이 되었을때 손을 편상태, 0.05 보다 작을때는 주먹을 쥔 상태가 된다
    - 이때 middleFingerMetacarpal를 기준으로 오른 손의 위치를 업데이트 하여 rightTransform에 업데이트 해주고, 이 값을 이용해 포물선을 그려준다
    - 왼손의 경우 오른손으로 잡은 위치인 rightTransform을 기준으로 왼손으로 포물선을 그려준다

--- APPRO/APPRO/Views/Stretchings/Shoulder/ShoulderStretchingViewModel+HandTraking.swift
+++ APPRO/APPRO/Views/Stretchings/Shoulder/ShoulderStretchingViewModel+HandTraking.swift
@@ -54,21 +54,87 @@ extension ShoulderStretchingViewModel {
         return beforeTransform
     }
     
+    func computeTransformHandTracking() {
+        // 오른손 계산
+        if !isRightDone {
+            guard let rightHandAnchor = latestHandTracking.right,
+                  rightHandAnchor.isTracked else {
+                return
+            }
+            
+            //right
+            rightThumbIntermediateBaseModelEntity.transform = getTransform(rightHandAnchor, .thumbIntermediateBase, rightThumbIntermediateBaseModelEntity.transform)
+            rightIndexFingerTipModelEntity.transform = getTransform(rightHandAnchor, .indexFingerTip, rightIndexFingerTipModelEntity.transform)
+            
+            rightRocketEntity.transform = getTransform(rightHandAnchor, .middleFingerMetacarpal, rightRocketEntity.transform)
+            
+            if !isFirstPositioning {
+                if !isFistShowing  {
+                    resetModelEntities()
+                    // TODO: RightTransform 위치를 기준으로 포물선 생성
+                    isFistShowing = true
+                }
+                return
+            }
+            
+            guard
+                let jointA = rightHandAnchor.handSkeleton?.joint(.thumbIntermediateBase),
+                let jointB = rightHandAnchor.handSkeleton?.joint(.indexFingerIntermediateTip) else { return  }
+            
+            guard jointA.isTracked && jointB.isTracked else { return }
+            
+            let jointATransfrom = matrix_multiply(
+                rightHandAnchor.originFromAnchorTransform, jointA.anchorFromJointTransform
+            ).columns.3.xyz
+            
+            let jointBTransfrom = matrix_multiply(
+                rightHandAnchor.originFromAnchorTransform, jointB.anchorFromJointTransform
+            ).columns.3.xyz
+            
+            let fingersDistance = distance(jointATransfrom, jointBTransfrom)
+            
+            if isFistShowing && fingersDistance > 0.1 {
+                isFistShowing = false
+            } else if !isFistShowing && fingersDistance < 0.05 {
+                resetModelEntities()
+                guard
+                    let fistJoint = rightHandAnchor.handSkeleton?.joint(.middleFingerMetacarpal),
+                    fistJoint.isTracked else {
+                    return
+                }
+                let mulitypliedMatrix = matrix_multiply(
+                    rightHandAnchor.originFromAnchorTransform, fistJoint.anchorFromJointTransform
+                )
+                self.rightHandTransform = Transform(matrix: mulitypliedMatrix)
+                
+                // TODO: RightTransform 위치를 기준으로 포물선 생성
+                isFistShowing = true
+            } else {
+                return
+            }
+            
+        }
+        // 왼손 계산
+        else {
+            guard let leftHandAnchor = latestHandTracking.left,
+                  leftHandAnchor.isTracked else {
+                return
+            }
+            //left
+            leftIndexFingerIntermediateBaseModelEntity.transform = getTransform(leftHandAnchor, .thumbIntermediateBase, leftIndexFingerIntermediateBaseModelEntity.transform)
+            leftIndexFingerTipModelEntity.transform = getTransform(leftHandAnchor, .indexFingerTip, leftIndexFingerTipModelEntity.transform)
+            leftRocketEntity.transform = getTransform(leftHandAnchor, .middleFingerMetacarpal, leftRocketEntity.transform)
+            leftRocketEntity.transform.rotation *= simd_quatf(angle: .pi, axis: SIMD3<Float>(0, 1, 0))
+            
+            
+            if !isFistShowing {
+                resetModelEntities()
+                
+                // TODO: RightTransform 위치를 기준으로 포물선 생성
+                isFistShowing = true
+            }
+        }
+    }
+    
 }
 
-private extension ModelEntity {
-    static func createHandEntity(isMarker: Bool = false)  -> ModelEntity {
-        var modelEntity = ModelEntity()
-        var clearMaterial = PhysicallyBasedMaterial()
-        clearMaterial.blending = .transparent(opacity: PhysicallyBasedMaterial.Opacity(scale: 0))
-        if isMarker {
-            modelEntity = ModelEntity(mesh: .generateSphere(radius: 0.05), materials: [clearMaterial])
-            modelEntity.generateCollisionShapes(recursive: true)
-            modelEntity.name = "Marker"
-        } else {
-            modelEntity = ModelEntity(mesh: .generateBox(size: 0.012), materials: [clearMaterial])
-            modelEntity.name = "Finger"
-        }
-        return modelEntity
-    }
-}
--- APPRO/APPRO/Views/Stretchings/Shoulder/ShoulderStretchingViewModel.swift
+++ APPRO/APPRO/Views/Stretchings/Shoulder/ShoulderStretchingViewModel.swift
@@ -14,6 +14,10 @@ import ARKit
 @MainActor
 final class ShoulderStretchingViewModel {
     
+    var contentEntity = Entity()
+    var modelEntities: [Entity] = []
+    var handEntities: [Entity] = []
+    
     let session = ARKitSession()
     var handTrackingProvider = HandTrackingProvider()
     var latestHandTracking: HandsUpdates = .init(left: nil, right: nil)
@@ -25,5 +29,34 @@ final class ShoulderStretchingViewModel {
     var leftIndexFingerIntermediateBaseModelEntity = ModelEntity.createHandEntity()
     var leftIndexFingerTipModelEntity = ModelEntity.createHandEntity()
     var leftRocketEntity = ModelEntity.createHandEntity(isMarker: true)
+    
+    var isFistShowing: Bool = false
+    var isFirstPositioning: Bool = true
+    var isRightDone: Bool = false
+    var rightHandTransform = Transform()
+    
+    func resetModelEntities() {
+        modelEntities.forEach { entity in
+            entity.removeFromParent()
+        }
+        modelEntities = []
+    }
+
 }
 
+private extension ModelEntity {
+    static func createHandEntity(isMarker: Bool = false)  -> ModelEntity {
+        var modelEntity = ModelEntity()
+        var clearMaterial = PhysicallyBasedMaterial()
+        clearMaterial.blending = .transparent(opacity: PhysicallyBasedMaterial.Opacity(scale: 0))
+        if isMarker {
+            modelEntity = ModelEntity(mesh: .generateSphere(radius: 0.05), materials: [clearMaterial])
+            modelEntity.generateCollisionShapes(recursive: true)
+            modelEntity.name = "Marker"
+        } else {
+            modelEntity = ModelEntity(mesh: .generateBox(size: 0.012), materials: [clearMaterial])
+            modelEntity.name = "Finger"
+        }
+        return modelEntity
+    }
+}
