371dda22ebbd34068e01b2418fce3bd80204fc6e
daminoworld <ekadls757@gmail.com>
2024-10-31 15:38:02 +0900

    Add: Add Private ModelEntity extension

--- APPRO/APPRO/Views/Stretchings/Shoulder/ShoulderStretchingViewModel+HandTraking.swift
+++ APPRO/APPRO/Views/Stretchings/Shoulder/ShoulderStretchingViewModel+HandTraking.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+private extension ModelEntity {
+    static func createHandEntity(isMarker: Bool = false)  -> ModelEntity {
+        var modelEntity = ModelEntity()
+        var clearMaterial = PhysicallyBasedMaterial()
+        clearMaterial.blending = .transparent(opacity: PhysicallyBasedMaterial.Opacity(scale: 0))
+        if isMarker {
+            modelEntity = ModelEntity(mesh: .generateSphere(radius: 0.05), materials: [clearMaterial])
+            modelEntity.generateCollisionShapes(recursive: true)
+            modelEntity.name = "Marker"
+        } else {
+            modelEntity = ModelEntity(mesh: .generateBox(size: 0.012), materials: [clearMaterial])
+            modelEntity.name = "Finger"
+        }
+        return modelEntity
+    }
+}
