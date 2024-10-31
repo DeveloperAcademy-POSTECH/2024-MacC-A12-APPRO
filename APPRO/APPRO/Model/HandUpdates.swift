7ca96c4801561c4cdd6a6177759acf64f4510a4d
daminoworld <ekadls757@gmail.com>
2024-10-31 14:32:02 +0900

    Add: Add HandUpdate for each hand anchor

---
+++ APPRO/APPRO/Model/HandUpdates.swift
@@ -0,0 +1,14 @@
+//
+//  HandUpdates.swift
+//  APPRO
+//
+//  Created by Damin on 10/31/24.
+//
+
+import Foundation
+import ARKit
+
+struct HandsUpdates {
+    var left: HandAnchor?
+    var right: HandAnchor?
+}
