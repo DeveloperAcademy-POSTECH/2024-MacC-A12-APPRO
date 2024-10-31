67683f41cf1c143a50b520e268b186e3a2130399
daminoworld <ekadls757@gmail.com>
2024-10-31 15:38:55 +0900

    Chore: Add Hand Tracking Properties

--- APPRO/APPRO/Views/Stretchings/Shoulder/ShoulderStretchingViewModel.swift
+++ APPRO/APPRO/Views/Stretchings/Shoulder/ShoulderStretchingViewModel.swift
@@ -5,4 +5,25 @@
 //  Created by Damin on 10/31/24.
 //
 
-import Foundation
+import SwiftUI
+import RealityKit
+import RealityKitContent
+import ARKit
+
+@Observable
+@MainActor
+final class ShoulderStretchingViewModel {
+    
+    let session = ARKitSession()
+    var handTrackingProvider = HandTrackingProvider()
+    var latestHandTracking: HandsUpdates = .init(left: nil, right: nil)
+    // right
+    var rightThumbIntermediateBaseModelEntity = ModelEntity.createHandEntity()
+    var rightIndexFingerTipModelEntity = ModelEntity.createHandEntity()
+    var rightRocketEntity = ModelEntity.createHandEntity(isMarker: true)
+    // left
+    var leftIndexFingerIntermediateBaseModelEntity = ModelEntity.createHandEntity()
+    var leftIndexFingerTipModelEntity = ModelEntity.createHandEntity()
+    var leftRocketEntity = ModelEntity.createHandEntity(isMarker: true)
+}
+
