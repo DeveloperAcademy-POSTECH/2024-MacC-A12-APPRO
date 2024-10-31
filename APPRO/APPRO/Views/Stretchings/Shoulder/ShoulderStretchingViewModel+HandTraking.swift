9ccefbd1701a0cf428ed4e31fe9f77aea60cd306
daminoworld <ekadls757@gmail.com>
2024-10-31 15:40:37 +0900

    Feat: Add ViewModel HandTracking code

--- APPRO/APPRO/Views/Stretchings/Shoulder/ShoulderStretchingViewModel+HandTraking.swift
+++ APPRO/APPRO/Views/Stretchings/Shoulder/ShoulderStretchingViewModel+HandTraking.swift
@@ -5,7 +5,57 @@
 //  Created by Damin on 10/31/24.
 //
 
-import Foundation
+import RealityKit
+import ARKit
+import SwiftUI
+
+extension ShoulderStretchingViewModel {
+    func startHandTrackingSession() async {
+        do {
+            if HandTrackingProvider.isSupported {
+                try await session.run([handTrackingProvider])
+            }
+        } catch {
+            print("ARKitSession error:", error)
+        }
+    }
+    
+    func updateHandTracking() async {
+        for await update in handTrackingProvider.anchorUpdates {
+            switch update.event {
+            case .updated:
+                let anchor = update.anchor
+                if !anchor.isTracked {
+                    continue
+                }
+                if anchor.chirality == .left {
+                    self.latestHandTracking.left = anchor
+                } else if anchor.chirality == .right {
+                    self.latestHandTracking.right = anchor
+                }
+            default:
+                break
+            }
+        }
+    }
+    
+    func getTransform(_ anchor: HandAnchor, _ jointName: HandSkeleton.JointName, _ beforeTransform: Transform) -> Transform {
+        let joint = anchor.handSkeleton?.joint(jointName)
+        
+        if ((joint?.isTracked) != nil) {
+            //MARK: 두 변환을 결합하여 손의 특정 관절이 월드 좌표계에서 정확히 어디에 위치하는지를 구하기 위함입니다.
+            let t = matrix_multiply(anchor.originFromAnchorTransform, (joint?.anchorFromJointTransform)!)
+            var transform = Transform(matrix: t)
+            
+            // 로켓의 transform을 적용
+            transform.scale = SIMD3<Float>(repeating: 0.1)
+            return transform
+        }
+        return beforeTransform
+    }
+    
+}
+
 private extension ModelEntity {
     static func createHandEntity(isMarker: Bool = false)  -> ModelEntity {
         var modelEntity = ModelEntity()
