bde746b97198d5934c2d204e02031c609cea8c18
daminoworld <ekadls757@gmail.com>
2024-11-05 19:06:32 +0900

    Add: Add ShoulderStretchingTutorialView
    - 기존 ShoulderStretchingView에서 튜토리얼에서 변경이 필요한 일부 로직만 빼고 그대로 추가

---
+++ APPRO/APPRO/Views/Tutorial/Shoulder/ShoulderStretchingTutorialView.swift
@@ -0,0 +1,100 @@
+//
+//  ShoulderStretchingTutorialView.swift
+//  APPRO
+//
+//  Created by Damin on 11/4/24.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ShoulderStretchingTutorialView: View {
+    @Environment(AppState.self) var appState: AppState
+    @State private var viewModel = ShoulderStretchingViewModel()
+    @State private var isColliding: Bool = false
+    
+    var body: some View {
+        RealityView { content in
+            content.add(viewModel.contentEntity)
+        } update: { content in
+        }
+        .onAppear() {
+        }
+        .upperLimbVisibility(.hidden)
+        .ignoresSafeArea()
+        .task {
+            await viewModel.loadStarModelEntity()
+        }
+        .task {
+            await viewModel.startHandTrackingSession()
+        }
+        .task {
+            await viewModel.updateHandTracking()
+        }
+    func subscribeToCollisionEvents(content: RealityViewContent) {
+        guard let rightCollisionModel = viewModel.handRocketEntity.findEntity(named: "RocketCollisionModel") as? ModelEntity else { return }
+
+            // 충돌 시작 감지
+        _ = content.subscribe(to: CollisionEvents.Began.self, on: rightCollisionModel) { collisionEvent in
+            setCollisionAction(collisionEvent: collisionEvent)
+        }
+        
+        // 충돌 종료 감지
+        _ = content.subscribe(to: CollisionEvents.Ended.self, on: rightCollisionModel) { collisionEvent in
+            handleCollisionEnd(collisionEvent: collisionEvent)
+        }
+        
+        // 애니메이션 종료 감지
+        _ = content.subscribe(to: AnimationEvents.PlaybackCompleted.self, on: nil) { animationEvent in
+            if animationEvent.playbackController.entity?.name == "EntryRocket" {
+                viewModel.entryRocketEntity.removeFromParent()
+                viewModel.addRightHandAnchor()
+            }else {
+                animationEvent.playbackController.entity?.removeFromParent()
+                
+            }
+        }
+    }
+    
+    func setCollisionAction(collisionEvent: CollisionEvents.Began) {
+        
+        let collidedModelEntity = collisionEvent.entityB
+        
+        if collidedModelEntity.name.contains("Timer") && !isColliding {
+            viewModel.playAnimation(animationEntity: viewModel.shoulderTimerEntity)
+            isColliding = true
+            return
+        }
+        
+        let entityName = viewModel.isRightDone ? "leftModelEntity" : "rightModelEntity"
+        
+        // 충돌시 particle, audio 실행
+        viewModel.playEmitter(eventEntity: collidedModelEntity)
+        Task {
+            await viewModel.playSpatialAudio(collidedModelEntity)
+        }
+        // 다음 엔터티 일때만 Material 변경
+        if collidedModelEntity.name == "\(entityName)-\(viewModel.expectedNextNumber)" {
+            viewModel.changeMatreialColor(entity: collidedModelEntity)
+            viewModel.addExpectedNextNumber()
+            
+            // 마지막 엔터티 감지
+            if collidedModelEntity.name.contains("\(viewModel.numberOfObjects - 2)") && currentStep == .step3{
+                viewModel.addShoulderTimerEntity()
+            }
+        }
+    }
+    
+    // 충돌 종료를 감지하여 타이머를 중지
+    func handleCollisionEnd(collisionEvent: CollisionEvents.Ended) {
+        let entityName = collisionEvent.entityB.name
+        if entityName.contains("Timer") {
+            viewModel.timerController?.stop()
+            isColliding = false
+        }
+    }
+}
+
+#Preview {
+    ShoulderStretchingView()
+}
