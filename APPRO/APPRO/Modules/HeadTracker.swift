276600ae52cf10afc9136253529d36a476f2d47d
Sangyun Jeong <jsangyun1039@naver.com>
2024-11-07 16:04:51 +0900

    Feat: HeadTracker 구현
    - HeadTracker
    - ClosureComponent
    - ClosureSystem

---
+++ APPRO/APPRO/Modules/HeadTracker.swift
@@ -0,0 +1,71 @@
+//
+//  HeadTracker.swift
+//  APPRO
+//
+//  Created by 정상윤 on 11/7/24.
+//
+
+import ARKit
+import RealityKit
+import QuartzCore
+
+final class HeadTracker: ObservableObject {
+    
+    let arSession = ARKitSession()
+    let worldTracking = WorldTrackingProvider()
+    
+    init() {
+        Task {
+            guard WorldTrackingProvider.isSupported else {
+                dump("WorldTrackingProvider is not supported on this device")
+                return
+            }
+            
+            do {
+                try await arSession.run([worldTracking])
+            } catch {
+                dump(error)
+            }
+        }
+    }
+    
+    deinit {
+        arSession.stop()
+    }
+    
+    func originFromDeviceTransform() -> simd_float4x4? {
+        guard let deviceAnchor = worldTracking.queryDeviceAnchor(atTimestamp: CACurrentMediaTime()) else { return nil }
+        
+        return deviceAnchor.originFromAnchorTransform
+    }
+    
+}
+
+
+struct ClosureComponent: Component {
+    
+    let closure: (TimeInterval) -> Void
+    
+    init(closure: @escaping (TimeInterval) -> Void) {
+        self.closure = closure
+        
+        ClosureSystem.registerSystem()
+    }
+    
+}
+
+struct ClosureSystem: System {
+    
+    static let query = EntityQuery(where: .has(ClosureComponent.self))
+    
+    init(scene: Scene) {}
+    
+    func update(context: SceneUpdateContext) {
+        for entity in context.entities(matching: Self.query, updatingSystemWhen: .rendering) {
+            guard let component = entity.components[ClosureComponent.self] else { return }
+            
+            component.closure(context.deltaTime)
+        }
+    }
+    
+}
