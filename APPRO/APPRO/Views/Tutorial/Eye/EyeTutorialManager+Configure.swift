1e23efd202dd9779f37a9844d6dbd2bb03cab647
Sangyun Jeong <jsangyun1039@naver.com>
2024-11-19 14:33:40 +0900

    Feat: CollisionComponent 코드 추가
    - generateShapeResource 메서드 구현
    - 눈, 링 CollisionComponent set 메서드 구현

--- APPRO/APPRO/Views/Tutorial/Eye/EyeTutorialManager+Configure.swift
+++ APPRO/APPRO/Views/Tutorial/Eye/EyeTutorialManager+Configure.swift
@@ -23,12 +23,49 @@ extension EyeTutorialManager {
         entity.findEntity(named: "patch")?.components.set(HoverEffectComponent(.highlight(.default)))
     }
     
+    func setEyeCollisionComponent(entity: Entity) async {
+        guard let leftEyeEntity = entity.findEntity(named: "eye_left"),
+              let rightEyeEntity = entity.findEntity(named: "eye_right") else {
+            dump("setEyeCollisionComponent failed: cannot find eye entities")
+            return
+        }
+        
+        guard let leftShapeResource = await generateShapeResource(entity: leftEyeEntity, isConvex: true),
+        let rightShapeResource = await generateShapeResource(entity: rightEyeEntity, isConvex: true) else {
+            return
+        }
+        leftEyeEntity.components.set(CollisionComponent(shapes: [leftShapeResource]))
+        rightEyeEntity.components.set(CollisionComponent(shapes: [rightShapeResource]))
+    }
+    
     func configureChickenEntity(entity: Entity) {
-        entity.setPosition(.init(x: 1.8, y: 0, z: 0), relativeTo: eyesEntity)
+        entity.setPosition(.init(x: -1.5, y: 0, z: 0), relativeTo: eyesEntity)
         entity.components.set(HoverEffectComponent(.highlight(.default)))
         entity.components.set(OpacityComponent(opacity: 0.0))
         entity.components.set(InputTargetComponent(allowedInputTypes: .indirect))
         entity.generateCollisionShapes(recursive: true)
+    }
+    
+    func configureRingEntity(entity: Entity) async {
+        ringEntity.components.set(OpacityComponent(opacity: 0))
+        ringEntity.setPosition(.init(x: 0, y: 0, z: 0), relativeTo: eyesEntity)
+        
+        await setRingCollisionComponent(entity: entity)
+    }
+    
+    private func setRingCollisionComponent(entity: Entity) async {
+        guard let innerPlaneEntity = ringEntity.findEntity(named: "inner_plane"),
+              let restrictLineEntity = ringEntity.findEntity(named: "restrict_line") else {
+            dump("configureRingChildrenEntities failed: No innerPlaneEntity or restrictLineEntity found")
+            return
+        }
+        
+        guard let innerPlaneShapeResource = await generateShapeResource(entity: innerPlaneEntity, isConvex: false),
+        let restrictLineShapeResource = await generateShapeResource(entity: restrictLineEntity, isConvex: false) else {
+            return
+        }
+        innerPlaneEntity.components.set(CollisionComponent(shapes: [innerPlaneShapeResource], isStatic: true))
+        restrictLineEntity.components.set(CollisionComponent(shapes: [restrictLineShapeResource], isStatic: true))
     }
     
     private func setClosureComponent(
@@ -48,6 +85,23 @@ extension EyeTutorialManager {
             entity.look(at: currentTranslation, from: newPosition, relativeTo: nil, forward: forwardDirection)
         }
         entity.components.set(closureComponent)
+    }
+    
+    private func generateShapeResource(entity: Entity, isConvex: Bool) async -> ShapeResource? {
+        guard let modelEntity = entity.children.filter({ $0 is ModelEntity }).first as? ModelEntity,
+              let mesh = modelEntity.model?.mesh else {
+            dump("generateShapeResourceByMesh failed: No mesh found in \(entity.name)")
+            return nil
+        }
+        
+        do {
+            return isConvex
+            ? try await ShapeResource.generateConvex(from: mesh)
+            : try await ShapeResource.generateStaticMesh(from: mesh)
+        } catch {
+            dump("generateShapeResourceByMesh failed: \(error)")
+            return nil
+        }
     }
     
 }
