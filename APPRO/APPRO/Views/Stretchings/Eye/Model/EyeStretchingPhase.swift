44ab5f7c97996888cd6da5e99c63165e8969f059
Sangyun Jeong <jsangyun1039@naver.com>
2024-11-23 16:07:05 +0900

    Add: EyeStretchingPhase enum 타입 구현

---
+++ APPRO/APPRO/Views/Stretchings/Eye/Model/EyeStretchingPhase.swift
@@ -0,0 +1,16 @@
+//
+//  EyeStretchingPhase.swift
+//  APPRO
+//
+//  Created by 정상윤 on 11/23/24.
+//
+
+import Foundation
+
+enum EyeStretchingPhase: Equatable {
+    
+    case waiting
+    case started
+    case finished
+    
+}
