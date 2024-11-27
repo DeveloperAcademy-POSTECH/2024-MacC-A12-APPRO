af2dd6f6f0cc62be34fe72f2e39faa10dfa7db28
daminoworld <ekadls757@gmail.com>
2024-11-27 19:14:09 +0900

    Feat: Implement SoundEffectHelper
    - SoundEffectProtocol을 만들고
    - 확장은 RawRepresentable을 채택한 타입에 대해서만 적용(RawRepresentable은 열거형이나 구조체가 원시 값을 가질 수 있게 하는 프로토콜)
    - RawRepresentable을 채택한 타입에서 원시 값이 String 타입인 경우에만 확장이 적용
    - SoundHelper 클래스를 SoundEffectProtocol을 준수하는 제네릭 타입을 받아서 사용
    - init시 제네릭 타입으로 받은 타입 enum을 통해 AudioFileResources를 로드하고, 로드 한값을 loadedSounds 딕셔너리에 추가
    - 재생시, effect 타입과 엔터티를 받아서 로드한 resource를 가져와 재생

---
+++ APPRO/APPRO/Views/Tutorial/Shoulder/SoundEffectHelper.swift
@@ -0,0 +1,78 @@
+//
+//  SpatialAudioHelper.swift
+//  APPRO
+//
+//  Created by Damin on 11/27/24.
+//
+
+import Foundation
+import RealityKit
+
+protocol SoundEffectProtocol: Hashable {
+    static var allCases: [Self] { get }
+    var fileName: String { get }
+    func playSound(on entity: Entity, resource: AudioFileResource)
+}
+
+extension SoundEffectProtocol where Self: RawRepresentable, Self.RawValue == String {
+    var fileName: String { rawValue }
+    
+    func playSound(on entity: Entity, resource: AudioFileResource) {
+        /// SpatailAudioComponent 적용
+        entity.components.set(SpatialAudioComponent())
+        let audioController = entity.prepareAudio(resource)
+        audioController.play()
+    }
+}
+
+// SoundHelper 클래스 정의
+final class SoundEffectHelper<T: SoundEffectProtocol> {
+    private var loadedSounds: [T: AudioFileResource] = [:]
+    private let queue = DispatchQueue(label: "SoundEffectHelper")
+
+    init() {
+        Task {
+            await loadSounds()
+        }
+    }
+    
+    private func loadSounds() async {
+        for effect in T.allCases {
+            do {
+                let resource = try await AudioFileResource(named: "\(effect.fileName).wav")
+                queue.sync { [weak self] in
+                    self?.loadedSounds[effect] = resource
+                }
+            } catch {
+                debugPrint("Failed to load sound: \(effect.fileName), \(error)")
+            }
+        }
+    }
+    
+    func playSound(_ effect: T, on entity: Entity, offset: Duration? = nil) {
+        queue.sync { [weak self] in
+            guard let resource = self?.loadedSounds[effect] else {
+                debugPrint("Sound not loaded: \(effect.fileName)")
+                return
+            }
+            effect.playSound(on: entity, resource: resource)
+        }
+    }
+}
+
+// 부위별 SoundEffect Enum 정의
+enum ShoulderSoundEffects: String, SoundEffectProtocol, CaseIterable {
+    case star1, star2, star3, star4, star5, star6
+    case entryRocket
+    case shoulderTimer
+}
+//TODO: 손목, 눈 예시
+enum WristSoundEffects: String, SoundEffectProtocol, CaseIterable {
+    case ringCharge, spiralHit, wristStretch
+}
+
+enum EyeSoundEffects: String, SoundEffectProtocol, CaseIterable {
+    case focusGain, distractionCut, eyeStretch
+}
+
+
